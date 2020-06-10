1d1aaf5483d0881d39ee285fe6d4790ed8669920
baseifert <baseifert@ethz.ch>
2020-06-10 14:19:48 +0200

    Updatet applications.
    Signed-off-by: baseifert <baseifert@ethz.ch>

--- applicationExample.m
+++ applicationExample.m
@@ -20,5 +20,26 @@ triangleTransformedPart(szN/4*szN/4:end) = 0;
 %transform to space domain again
 J(idx) = uint8(dtt\triangleTransformedPart);
 %compare the filtered with the non-filtered part
+% figure;
+% montage([firstJ, J]);
+
+dot = createOrthoTriangleDCT(szN);
+%read an example image
+orthoJ = imresize(imread('rice.png'), [szN szN]);
+%take one triangle of it
+idx = tril(ones(szN,szN))==1;
+%triangle DCT transform
+triangleTransformedPart = dot * double(orthoJ(idx));
+%throw away three quarter of the frequencies
+triangleTransformedPart(szN/2*szN/2:end) = 0;
+%transform to space domain again
+orthoJ(idx) = uint8(dot' * triangleTransformedPart);
+secondJ = orthoJ;
+
+%throw away 15/16 of the frequencies
+triangleTransformedPart(szN/4*szN/4:end) = 0;
+%transform to space domain again
+orthoJ(idx) = uint8(dot' * triangleTransformedPart);
+%compare the filtered with the non-filtered part
 figure;
-montage([firstJ, J]);
+montage([firstJ, J;secondJ, orthoJ]);
